8e53597591dbd3c46a893ec7b4372556ab170ebe
Kracken914dt <mecagisk6666@gmail.com>
2026-04-21 16:30:07 -0500

    ajuste 3.2

--- src/services/libs/ApiConfig.ts
+++ src/services/libs/ApiConfig.ts
@@ -1,7 +1,7 @@
 export const API_URLS = {
   Base: process.env.NEXT_PUBLIC_API_BASE ?? "http://localhost:5000",
   ImportExport:
-    process.env.NEXT_PUBLIC_IMPORT_EXPORT_API ??
+    process.env.NEXT_PUBLIC_IMPORT_EXPORT_API_BASE ??
     process.env.NEXT_PUBLIC_API_BASE ??
     "http://localhost:5000",
 };
